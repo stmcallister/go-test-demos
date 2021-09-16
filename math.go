684f0688162ffd120667704d1ee5cf703f6973ac
Scott McAllister <smcallister@pagerduty.com>
2021-09-16 14:49:46 -0500

    pushing remote

---
+++ math.go
@@ -0,0 +1,14 @@
+package main
+
+func add(num1, num2 float64) float64 {
+	return num1 + num2
+}
+func subtract(num1, num2 float64) float64 {
+	return num1 - num2
+}
+func multiply(num1, num2 float64) float64 {
+	return num1 * num2
+}
+func divide(num1, num2 int) float64 {
+	return float64(num1 / num2)
+}
---
+++ math_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func TestMathAdd1(t *testing.T) {
+// 	first := 111
+// 	second := 222
+// 	want := 333
+
+// 	got := add(first, second)
+
+// 	if got != want {
+// 		t.Fatalf(`Add %v plus %v returned %v. Wanted %v`, first, second, got, want)
+// 	}
+// }
+// func TestMathAdd2(t *testing.T) {
+// 	first := 245
+// 	second := 0
+// 	want := 245
+
+// 	got := add(first, second)
+
+// 	if got != want {
+// 		t.Fatalf(`Add %v plus %v returned %v. Wanted %v`, first, second, got, want)
+// 	}
+// }
+// func TestMathAdd3(t *testing.T) {
+// 	first := 333
+// 	second := 123
+// 	want := 456
+
+// 	got := add(first, second)
+
+// 	if got != want {
+// 		t.Fatalf(`Add %v plus %v returned %v. Wanted %v`, first, second, got, want)
+// 	}
+// }
+
+func TestAddAll(t *testing.T) {
+	var tests = []struct {
+		first, second, want float64
+	}{
+		{1, 2, 3},
+		{4, 5, 9},
+		{123, 500, 623},
+		{987, 111, 1098},
+		{5, 5, 10},
+	}
+
+	for _, tt := range tests {
+		// TODO: get notes on why this is necessary
+		tt := tt
+		testname := fmt.Sprintf("Add:%v,%v", tt.first, tt.second)
+
+		t.Run(testname, func(t *testing.T) {
+			got := add(tt.first, tt.second)
+
+			if got != tt.want {
+				t.Errorf(`Add %v plus %v should be %v. Got %v`, tt.first, tt.second, tt.want, got)
+			}
+		})
+	}
+	t.Run("integration", func(t *testing.T) {
+		first := 100.0
+		second := 200.0
+		want := 30000.0
+
+		got := multiply(add(first, second), subtract(second, first))
+		if got != want {
+			t.Errorf(`Should be %v. Got %v`, want, got)
+		}
+	})
+}
